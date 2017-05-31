1c2a1d67692055cc2da2ef701832558d90c5974f
wkzq <feng_sh@outlook.com>
2017-05-31 21:09:28 +0800

    test

--- src/main/java/xyz/fefine/controller/IndexController.java
+++ src/main/java/xyz/fefine/controller/IndexController.java
@@ -1,11 +1,8 @@
 package xyz.fefine.controller;
 
-import bid.fese.entity.SeRequest;
 import bid.fese.entity.SeResponse;
 import xyz.fefine.annotation.Path;
 import xyz.fefine.annotation.RequestParam;
-
-import java.io.IOException;
 
 /**
  * Created by feng_sh on 17-5-31.
@@ -15,24 +12,19 @@ public class IndexController {
 
     @Path("/index")
     public void index(SeResponse response) {
-        try {
-            response.getOutStream().write("<h2>this is index</h2>".getBytes());
-            response.flush();
-        } catch (IOException e) {
-            System.out.println("index show error");
-            e.printStackTrace();
-        }
+        response.getCookies().set("name", "feng");
+        response.getCookies().set("sex", "nan");
+        response.getPrintWriter().print(("<h2>index</h2>"));
+        response.getPrintWriter().flush();
+        response.flush();
     }
 
     @Path("/page/{num}")
     public void pageShow(@RequestParam("num") int num, SeResponse response) {
-        try {
-            response.getOutStream().write(("<h2>this is page "+ num + "</h2>").getBytes());
-            response.flush();
-        } catch (IOException e) {
-            System.out.println("page show error");
-            e.printStackTrace();
-        }
+        String name = response.getCookies().get("name");
+        response.getPrintWriter().print(("<h2>this is page "+ num + " and the name is " + name + "</h2>"));
+        response.getPrintWriter().flush();
+        response.flush();
     }
 
 }
